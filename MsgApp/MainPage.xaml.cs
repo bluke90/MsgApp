fce9a2ea1f0dc7330a22695cae5780b379f937b8
Blake Ganzerla <blake.ganzerla@outlook.com>
2021-11-22 14:55:49 -0600

    Add project files.

---
+++ MsgApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MsgApp
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			var un = Preferences.Get("un", "");
+
+			count++;
+			CounterLabel.Text = $"Username: {un}";
+
+			SemanticScreenReader.Announce(CounterLabel.Text);
+		}
+	}
+}
