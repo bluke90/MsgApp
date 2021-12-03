3e7293f2f63f126cf0c8f198e2b9d1da5a9e2b8e
Blake Ganzerla <blake.ganzerla@outlook.com>
2021-12-03 10:32:39 -0600

    Commit Project migration

---
+++ MsgApp_Client/Pages/MessageDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using MsgApp_Client.Models;
+using MsgApp_Client.Services;
+
+namespace MsgApp_Client.Pages
+{
+	public partial class MessageDialog : ContentPage
+	{
+		private Person Person = null;
+		private readonly DataContext _context = new DataContext();
+		public MessageDialog (Person person)
+		{
+			InitializeComponent();
+			Person = _context.People.FirstOrDefault(m => m.Id == person.Id);
+		}
+
+		private void GetMessages(object sender, EventArgs e)
+		{
+
+        }
+
+		private void OnSendMessage(object sender, EventArgs e)
+        {
+			// Get message from entry
+			var data = dataEntry.Text;
+			// Build Message
+
+
+        }
+
+	}
+}
---
+++ MsgApp_Client/Pages/SetupPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MsgApp_Client.Pages
+{
+	public partial class SetupPage : ContentPage
+	{
+
+		public SetupPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnGoClicked(object sender, EventArgs e) {
+			var un = userName.Text;
+
+            Preferences.Set("un", un);
+			App.Current.MainPage = new MainPage();
+
+        }
+
+	}
+}
